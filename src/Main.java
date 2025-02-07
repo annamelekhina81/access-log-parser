bc0e3524fa000dc37ae7bdde9115ce67ced2667c
Anna Melekhina <anna_2009_81@mail.ru>
2025-02-07 16:18:42 +0300

    Изменена логика метода main (простые действия с числами)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите Enter: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        System.out.print("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: "+ addingNumbers(firstNumber, secondNumber));
+        System.out.println("Разность чисел: "+ differenceNumbers(firstNumber, secondNumber));
+        System.out.println("Произведение чисел: "+ multiplicationNumbers(firstNumber, secondNumber));
+        System.out.println("Частное чисел: "+ quotientNumbers(firstNumber, secondNumber));
+    }
+    public static int addingNumbers(int x, int y) {
+        return x+y;
+    }
+
+    public static int differenceNumbers(int x, int y) {
+        return x-y;
+    }
+
+    public static int multiplicationNumbers(int x, int y) {
+        return x*y;
+    }
+    public static double quotientNumbers(int x, int y) {
+        return (double) x/y;
     }
 }
+
