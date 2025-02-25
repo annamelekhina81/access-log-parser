6d2043d8d9d43fdce9a16d10f41d1a992f90ca59
Anna Melekhina <anna_2009_81@mail.ru>
2025-02-25 08:21:39 +0300

    Курсовая работа. бесконечный цикл while, запрос пути к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.io.File;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите Enter: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        int count=0;
+
+        while (true){
+            String path = new Scanner(System.in).nextLine();
+            File file = new File (path);
+            if (file.exists() && !file.isDirectory()){
+                count++;
+                System.out.println("Путь указан верно. Это файл № "+ count );
+                continue;
+            }
+            else if ((!file.exists() && !file.isDirectory())) {
+                System.out.println("Указанного файла (директории) не существует или путь указан не верно");
+                continue;
+
+            }
+            else if (file.exists()&& file.isDirectory()){
+                System.out.println("Вы указали путь к директории. Введите путь к файлу");
+                continue;
+            }
+
+        }
+
     }
+
 }
+
+
