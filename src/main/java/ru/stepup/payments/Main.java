f6396923e3bccfb01ca869b46866d01b37ce130f
Anna Melekhina <anna_2009_81@mail.ru>
2025-03-26 17:02:01 +0300

    Курсовой проект 3. Подсчет статистики общей, средней за час и за указанный период из файла

---
+++ src/main/java/ru/stepup/payments/Main.java
@@ -0,0 +1,59 @@
+package ru.stepup.payments;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        ArrayList<LogEntry> logEntriesArrays = new ArrayList<>();
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            if (file.exists() && !file.isDirectory()) {
+                count++;
+                System.out.println("Путь указан верно. Это файл № " + count);
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader = new BufferedReader(fileReader);
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        if (length >= 1024) throw new ExceptionError("В файле длина строки более 1024 символов");
+                        line = line.replaceAll(" -", "");
+                        line = line.replaceAll("\\[", "");
+                        line = line.replaceAll("0300] ", "");
+                        line = line.replaceAll("[+]", "");
+                        LogEntry logEntry = new LogEntry(line);
+                        logEntriesArrays.add(logEntry);
+                        // System.out.println(logEntry.toString());
+                    }
+                    Statistics st = new Statistics();
+                    //System.out.println(logEntriesArrays);
+                    st.addEntry(logEntriesArrays);
+                    System.out.println("Полный объем трафика за весь период = " + st.getTotalTraffic());
+                    System.out.println("Средний объем трафика за час = " + st.getTotalTrafficHour());
+                    System.out.println("За выбранный период ");
+                    st.getTrafficRate(logEntriesArrays, LocalDate.of(2022, Month.SEPTEMBER, 25).atTime(6, 25, 5),
+                            LocalDate.of(2022, Month.SEPTEMBER, 25).atTime(6, 25, 7));
+                    System.out.println("Общий объем трафика за выбранный период: " + st.getMinTime() + " до " + st.getMaxTime() + " = " + st.getTotalTrafficPeriod());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                continue;
+            } else if ((!file.exists() && !file.isDirectory())) {
+                System.out.println("Указанного файла (директории) не существует или путь указан не верно");
+                continue;
+            } else if (file.exists() && file.isDirectory()) {
+                System.out.println("Вы указали путь к директории. Введите путь к файлу");
+                continue;
+            }
+        }
+    }
+}
+//C:\Users\AMelekhina\Documents\AT\access.log
---
+++ src/main/java/ru/stepup/payments/UserAgent.java
@@ -0,0 +1,56 @@
+package ru.stepup.payments;
+
+
+public class UserAgent {
+    private String str, os, browser;
+
+    UserAgent(String str) {
+        this.str = str;
+        setBrowser(str);
+        setOs(str);
+    }
+
+    public void setBrowser(String str) {
+        String[] browser = str.split(" ");
+        for (int i = 0; i <= browser.length - 1; i++) {
+            if (browser[i].contains("Chrome")) {
+                this.browser = "Chrome";
+            } else if (browser[i].contains("Opera")) {
+                this.browser = "Opera";
+            } else if (browser[i].contains("Firefox")) {
+                this.browser = "Firefox";
+            } else if (browser[i].contains("Edge")) {
+                this.browser = "Edge";
+            }
+        }
+    }
+
+    public void setOs(String str) {
+        String[] os = str.split(" ");
+        for (int i = 0; i <= os.length - 1; i++) {
+            if (os[i].contains("Windows")) {
+                this.os = "Windows";
+            } else if (os[i].contains("Macintosh")) {
+                this.os = "Macintosh";
+            } else if (os[i].contains("Linux")) {
+                this.os = "Linux";
+            }
+        }
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAgent{" +
+                ", os='" + getOs() + '\'' +
+                ", browser='" + getBrowser() + '\'' +
+                '}';
+    }
+}
