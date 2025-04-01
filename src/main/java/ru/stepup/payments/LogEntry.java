b9097e9eba9771392c158e052f110bfb6524a6bf
Anna Melekhina <anna_2009_81@mail.ru>
2025-04-01 22:08:42 +0300

    Курсовой проект. Статистика посещений и ошибочных запросов

---
+++ src/main/java/ru/stepup/payments/LogEntry.java
@@ -0,0 +1,70 @@
+package ru.stepup.payments;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class LogEntry {
+    String str;
+    private final String ip, method, urlRequest, referer;
+    private final LocalDateTime data;
+    private final int response, amount;
+    UserAgent userAgent;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy':'HH:mm:ss", Locale.ENGLISH);
+
+    public LogEntry(String str) {
+        this.str = str;
+        String[] parts = str.split(" ");
+        String[] parts2 = str.split("\"");
+        this.ip = parts[0];
+        this.data = LocalDateTime.parse(parts[1], formatter);
+        this.method = parts[2].replaceAll("\"", "");
+        this.urlRequest = parts[3];
+        this.response = Integer.parseInt(parts[5]);
+        this.amount = Integer.parseInt(parts[6]);
+        this.referer = parts[7].replaceAll("\"", "");
+        UserAgent ua = new UserAgent(parts2[5]);
+        this.userAgent = ua;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUrlRequest() {
+        return urlRequest;
+    }
+
+    public String getReferer() {
+        return referer;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public int getResponse() {
+        return response;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+
+    @Override
+    public String toString() {
+        return "LogEntry{" +
+                "log=" + getIp() + " "
+                + getData() + " " + getMethod() + " "
+                + getUrlRequest() + " " + getResponse() + " "
+                + getAmount() + " " + getReferer() +
+                " " + userAgent +
+                '}';
+    }
+}
---
+++ src/main/java/ru/stepup/payments/Statistics.java
@@ -0,0 +1,159 @@
+package ru.stepup.payments;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Statistics {
+    LogEntry logEntry;
+    private long totalTraffic, totalTrafficPeriod, totalTrafficHour, hours;
+    private LocalDateTime minTime, maxTime;
+    private final HashSet<String> pageOk = new HashSet<>();
+    private final HashSet<String> pageError = new HashSet<>();
+    private final HashMap<String, Integer> os = new HashMap<>();
+    private final HashMap<String, Integer> browser = new HashMap<>();
+    private final HashMap<String, Double> osStatictic = new HashMap<>();
+    private final HashMap<String, Double> browserStat = new HashMap<>();
+    //ArrayList<LogEntry> logEntriesArrays = new ArrayList<>();
+
+    public Statistics() {
+    }
+
+    public void addEntry(ArrayList<LogEntry> logEntryArr) {
+        //this.logEntriesArrays=new ArrayList<>(Arrays.asList(logEntry));
+        long sumAmount = 0;
+        this.minTime = logEntryArr.get(0).getData();
+        this.maxTime = logEntryArr.get(logEntryArr.size() - 1).getData();
+        this.hours = Duration.between(this.minTime, this.maxTime).toHours();
+        for (int i = 0; i < logEntryArr.size() - 1; i++) {
+            sumAmount += logEntryArr.get(i).getAmount();
+            if (logEntryArr.get(i).getResponse() == 200) {
+                pageOk.add(logEntryArr.get(i).getUrlRequest());
+            }
+            if (logEntryArr.get(i).getResponse() == 404 || logEntryArr.get(i).getResponse() == 500) {
+                pageError.add(logEntryArr.get(i).getUrlRequest());
+            }
+            if (os.containsKey(logEntryArr.get(i).userAgent.getOs())) {
+                os.put(logEntryArr.get(i).userAgent.getOs(), os.get(logEntryArr.get(i).userAgent.getOs()) + 1);
+            } else {
+                os.put(logEntryArr.get(i).userAgent.getOs(), 1);
+            }
+            if (browser.containsKey(logEntryArr.get(i).userAgent.getBrowser())) {
+                browser.put(logEntryArr.get(i).userAgent.getBrowser(), browser.get(logEntryArr.get(i).userAgent.getBrowser()) + 1);
+            } else {
+                browser.put(logEntryArr.get(i).userAgent.getBrowser(), 1);
+            }
+        }
+        this.totalTraffic = sumAmount;
+        this.totalTrafficHour = totalTraffic / hours;
+    }
+
+
+    public void getTrafficRate(ArrayList<LogEntry> logEntryArr, LocalDateTime minTime, LocalDateTime maxTime) {
+        ArrayList<LogEntry> logEntryArr2 = new ArrayList<>();
+        int sum = 0;
+        for (int i = 0; i < logEntryArr.size() - 1; i++) {
+            if ((logEntryArr.get(i).getData().isEqual(minTime) || logEntryArr.get(i).getData().isAfter(minTime))
+                    && ((logEntryArr.get(i).getData().isEqual(maxTime) || logEntryArr.get(i).getData().isBefore(maxTime)))) {
+                logEntryArr2.add(logEntryArr.get(i));
+            }
+        }
+        for (int i = 0; i < logEntryArr2.size() - 1; i++) {
+            System.out.println(logEntryArr2.get(i));
+            sum += logEntryArr2.get(i).getAmount();
+        }
+        this.totalTrafficPeriod = sum;
+    }
+
+    public long numberOfVisitsPerHour() {
+        int s = 0;
+        for (Map.Entry<String, Integer> entry : browser.entrySet()) {
+            if (!browser.containsKey("null") || !browser.containsKey("Bot")) {
+                s += entry.getValue();
+            }
+        }
+        return s / hours;
+    }
+
+    public long numbersOfErrorsRequest() {
+        return pageError.size() / hours;
+    }
+
+    public double averageAttendanceUser(ArrayList<LogEntry> logEntryArr) {
+        int s = 0;
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < logEntryArr.size() - 1; i++) {
+            if (!Objects.isNull(logEntryArr.get(i).userAgent.getBrowser())
+                    && !Objects.equals(logEntryArr.get(i).userAgent.getBrowser(), "Bot")) {
+                map.put(logEntryArr.get(i).getIp(), logEntryArr.get(i).userAgent.getBrowser());
+                s = s + 1;
+            }
+        }
+        return (double) map.size() / s;
+    }
+
+    public HashSet<String> getPageOk() {
+        return pageOk;
+    }
+
+    public HashMap<String, Integer> getOs() {
+        return os;
+    }
+
+    public HashMap<String, Double> getOsStatictic() {
+        int sumOs = os.values().stream().mapToInt(Integer::intValue).sum();
+        for (Map.Entry<String, Integer> entry : os.entrySet()) {
+            osStatictic.put(entry.getKey(), (double) entry.getValue() / sumOs);
+        }
+        return osStatictic;
+    }
+
+    public HashSet<String> getPageError() {
+        return pageError;
+    }
+
+    public HashMap<String, Integer> getBrowser() {
+        return browser;
+    }
+
+    public HashMap<String, Double> getBrowserStat() {
+        double sumBr = browser.values().stream().mapToInt(Integer::intValue).sum();
+        for (Map.Entry<String, Integer> entry : browser.entrySet()) {
+            browserStat.put(entry.getKey(), entry.getValue() / sumBr);
+        }
+        return browserStat;
+    }
+
+    public long getTotalTraffic() {
+        return totalTraffic;
+    }
+
+    public long getTotalTrafficPeriod() {
+        return totalTrafficPeriod;
+    }
+
+    public long getTotalTrafficHour() {
+        return totalTrafficHour;
+    }
+
+    public LocalDateTime getMinTime() {
+        return minTime;
+    }
+
+    public LocalDateTime getMaxTime() {
+        return maxTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Statistics{" +
+                "logEntry=" + logEntry +
+                ", totalTraffic=" + getTotalTraffic() +
+                ", minTime=" + getMinTime() +
+                ", maxTime=" + getMaxTime() +
+                '}';
+    }
+}
+
